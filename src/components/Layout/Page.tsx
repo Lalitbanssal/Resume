ccb68535d6edd33c9a8e52ea0fd22062590b7829
Lalitbanssal <153372672+Lalitbanssal@users.noreply.github.com>
2026-05-09 20:41:28 +0530

    change pages.tsx files

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -8,6 +8,9 @@ import {HomepageMeta} from '../../data/dataDef';
 const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
   const {asPath: pathname} = useRouter();
 
+  // Your custom domain setup
+  const baseUrl = 'https://lalitbansal.me';
+
   return (
     <>
       <Head>
@@ -15,7 +18,7 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <meta content={description} name="description" />
 
         {/* several domains list the same content, make sure google knows we mean this one. */}
-        <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
+        <link href={`${baseUrl}${pathname}`} key="canonical" rel="canonical" />
 
         <link href="/favicon.ico" rel="icon" sizes="any" />
         <link href="/icon.svg" rel="icon" type="image/svg+xml" />
@@ -25,11 +28,15 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
-        <meta content={`https://reactresume.com${pathname}`} property="og:url" />
+        <meta content={`${baseUrl}${pathname}`} property="og:url" />
+        {/* Your new Open Graph Image! */}
+        <meta content={`${baseUrl}/og-image.jpg`} property="og:image" />
 
         {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
+        <meta content="summary_large_image" name="twitter:card" />
         <meta content={title} name="twitter:title" />
         <meta content={description} name="twitter:description" />
+        <meta content={`${baseUrl}/og-image.jpg`} name="twitter:image" />
       </Head>
       {children}
     </>
